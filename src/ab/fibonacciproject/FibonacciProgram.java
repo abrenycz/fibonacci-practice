6ce19bfa936b4a5dc9fe75f906e7af0313cdb449
abrenycz <abrenycz@gmail.com>
2018-03-17 12:28:45 -0500

    added iterative fibonacci solution & tests

--- src/ab/fibonacciproject/FibonacciProgram.java
+++ src/ab/fibonacciproject/FibonacciProgram.java
@@ -1,15 +1,42 @@
 package ab.fibonacciproject;
 
+import java.util.ArrayList;
+
 public class FibonacciProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
-	public static int[] fibonacci(int i) {
-		// TODO Auto-generated method stub
-		return null;
+	public static ArrayList<Integer> fibonacci(int input) {
+		if(input==0)
+			return new ArrayList<Integer>();
+		else if(input==1)
+			return new ArrayList<Integer>(0);
+		else if(input==2) {
+			ArrayList<Integer> output = new ArrayList<Integer>();
+			output.add(0);
+			output.add(1);
+			return output;
+		}
+		else {
+			ArrayList<Integer> output = new ArrayList<Integer>();
+			
+			output.add(0);
+			output.add(1);
+			
+			int num1 = 0;
+			int num2 = 1;
+			int num3;
+			
+			for(int i = 2; i < input; i++) {
+				num3 = num1 + num2;
+				output.add(num3);
+				num1 = num2;
+				num2 = num3;
+			}
+			return output;
+		}
 	}
 
 }
--- src/ab/fibonacciproject/FibonacciTest.java
+++ src/ab/fibonacciproject/FibonacciTest.java
@@ -1,18 +1,25 @@
 package ab.fibonacciproject;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class FibonacciTest {
 	@Test
 	public void testFibonacci_OutToZero_isEmpty() {
-		int[] solution = {};
-		Assert.assertArrayEquals(FibonacciProgram.fibonacci(0), solution);
+		ArrayList<Integer> solution = new ArrayList<Integer>();
+		Assert.assertEquals(FibonacciProgram.fibonacci(0), solution);
 	}
 	
 	@Test
 	public void testFibonacci_OutToFive() {
-		int[] solution = {0,1,1,2,3};
-		Assert.assertArrayEquals(FibonacciProgram.fibonacci(5), solution);
+		ArrayList<Integer> solution = new ArrayList<Integer>();
+		solution.add(0);
+		solution.add(1);
+		solution.add(1);
+		solution.add(2);
+		solution.add(3);
+		Assert.assertEquals(FibonacciProgram.fibonacci(5), solution);
 	}
 }
